c7e414c86531175067e1c9fb88fef1ef74ee0567
Dennis Peter <peterdennis573@gmail.com>
2024-01-31 10:18:47 +0300

    First commit

---
+++ otp/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen mx-auto max-w-2xl flex text-center justify-center">
+      <div className="h-400 w-400  flex justify-center items-center max-w-2xl mx-auto">
+        <input
+          type="text"
+          class="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+          maxLength="1"/>
+          <input
+          type="text"
+          class="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+          maxLength="1"/>
+          <input
+          type="text"
+          class="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+          maxLength="1"/>
+          <input
+          type="text"
+          class="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+          maxLength="1"/>
+          <input
+          type="text"
+          class="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+          maxLength="1"/>
+          <input
+          type="text"
+          class="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+          maxLength="1"/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
