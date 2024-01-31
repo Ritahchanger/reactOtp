827de9a7ddc4827be3dabac475db3322cf31cdf8
Dennis Peter <peterdennis573@gmail.com>
2024-01-31 10:58:39 +0300

    Completed

--- otp/src/App.js
+++ otp/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const App = () => {
   const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const [formset, setFormset] = useState(false);
 
   const handleChange = (index, value) => {
     const newOtp = [...otp];
@@ -13,23 +14,42 @@ const App = () => {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const allBoxesFilled = otp.every((digit) => digit !== "");
+    if(allBoxesFilled){
+      setFormset(true);
+    }
+  };
+
   return (
-    <div className="h-screen flex-col mx-auto max-w-2xl flex text-center justify-center">
-     <form>
-     <div className="h-400 w-400 flex justify-center items-center max-w-2xl mx-auto">
-        {otp.map((digit, index) => (
+    <div className="h-screen flex mx-auto max-w-2xl flex-col items-center justify-center">
+      {!formset ? (
+        <form onSubmit={handleSubmit} className="flex justify-center items-center flex-col">
+          <div className="h-400 w-400 flex justify-center items-center max-w-2xl mx-auto">
+            {otp.map((digit, index) => (
+              <input
+                key={index}
+                type="text"
+                id={`otp-input-${index}`}
+                className="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
+                maxLength="1"
+                value={digit}
+                onChange={(e) => handleChange(index, e.target.value)}
+              />
+            ))}
+          </div>
           <input
-            type="text"
-            id={`otp-input-${index}`}
-            className="appearance-none w-12 h-12 mx-1 text-4xl border border-gray-300 rounded text-center focus:outline-none focus:border-indigo-500"
-            maxLength="1"
-            value={digit}
-            onChange={(e) => handleChange(index, e.target.value)}
+            type="submit"
+            value="SUBMIT"
+            className="my-10 p-4 bg-blue-500 mx-auto text-white rounded cursor-pointer transition-colors hover:bg-green-500"
           />
-        ))}
-      </div>
-      <input type="submit" value="SUBMIT" className="my-10 p-4 bg-blue-500 text-white rounded cursor-pointer"/>
-     </form>
+        </form>
+      ) : (
+        <div>
+          <h2>The form is set</h2>
+        </div>
+      )}
     </div>
   );
 };
